319113f0bcdc7066bc8f6d0783b4a54ca3069cb4
david <dgordon@uw.edu>
2019-03-01 17:02:01 -0800

    .snake to have lots of read naming types, add source_this_first.sh to run..., remove tmp_dir from .json

--- run_align_illumina_against_reference.snake.sh
+++ run_align_illumina_against_reference.snake.sh
@@ -1,2 +1,3 @@
 mkdir -p log
+source source_this_first.sh
 snakemake -s align_illumina_against_reference.snake --drmaa " -q eichler-short.q -l h_rt=24:00:00  -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash" -w 100 --jobs 100 -p
