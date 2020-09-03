8659a15658e3aea0b162b0d1df6164eed2135c1c
david <dgordon@uw.edu>
2020-09-03 15:32:18 -0700

    run_align_illumina_against_reference.snake.sh to rev modules up to centos7

--- run_align_illumina_against_reference.snake.sh
+++ run_align_illumina_against_reference.snake.sh
@@ -1,3 +1,8 @@
 mkdir -p log
-source source_this_first.sh
-snakemake -s align_illumina_against_reference.snake --jobname "{rulename}.{jobid}" --drmaa " -n w -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash" -w 100 --jobs 100 -p -k
+
+module purge
+module load modules modules-init modules-gs/prod modules-eichler/prod bedtools/2.29.0 miniconda/4.5.12
+module load bwa/0.7.17
+module load samtools/1.9
+
+snakemake -s align_illumina_against_reference.snake --drmaa " -w n -q eichler-short.q -l h_rt=24:00:00  -V -cwd -e ./log -o ./log {params.sge_opts}  -S /bin/bash" -w 100 --jobs 100 -p
